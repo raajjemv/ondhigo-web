c24d912e411f12983fed2d36bedcf283208660e8
raajjemv <36122879+raajjemv@users.noreply.github.com>
2020-12-01 09:20:06 +0500

    search

--- app/Http/Livewire/Products.php
+++ app/Http/Livewire/Products.php
@@ -9,11 +9,23 @@ use Livewire\WithPagination;
 class Products extends Component
 {
     use WithPagination;
-
+    public $search = '';
+    protected $queryString = ['search'];
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+    public function mount($search = null)
+    {
+        $this->search =  $search;
+    }
     public function render()
     {
         return view('livewire.products', [
-            'products' => Product::latest()->paginate(16)
+            'products' => Product::with('brand', 'category')
+                ->where('name', 'like', '%' . $this->search . '%')
+                ->latest()
+                ->paginate(16)
         ])->extends('layouts.main');;
     }
 }
