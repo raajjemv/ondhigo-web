06a5405d9c1ba867ea9deb667ce9177b16c85612
raajjemv <36122879+raajjemv@users.noreply.github.com>
2020-12-26 17:12:49 +0500

    Update about.blade.php

--- resources/views/livewire/about.blade.php
+++ resources/views/livewire/about.blade.php
@@ -30,7 +30,7 @@
                 <div class="w-full md:w-1/2 px-2 mt-5 md:mt-0 ">
                     <div class="bg-gray-50 p-5 rounded shadow">
                         <div class="side-line text-center text-3xl open-sans text-red-600 ">OUR
-                            VISSION</div>
+                            VISION</div>
                         <div class="mt-5 text-center text-gray-500">{!! $about->vission !!}</div>
                     </div>
                 </div>
