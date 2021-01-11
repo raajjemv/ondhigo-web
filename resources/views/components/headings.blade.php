a9bdf47af6bcbd1e3873b734143896805b90ad23
raajjemv <36122879+raajjemv@users.noreply.github.com>
2021-01-11 19:43:55 +0500

    Update headings.blade.php

--- resources/views/components/headings.blade.php
+++ resources/views/components/headings.blade.php
@@ -1,2 +1,3 @@
-<div class="my-5 {{ $color ? $color : 'text-red-700' }} text-5xl font-bold text-center py-3 open-sans">{{ $heading }}
+<div class="my-5 {{ $color ? $color : 'text-red-700' }} text-2xl md:text-5xl font-bold text-center py-3 open-sans">
+     {{ $heading }}
 </div>
