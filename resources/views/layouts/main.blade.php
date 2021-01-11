8c031185fc8a90a7315c19dd105f960b516f2f3c
raajjemv <36122879+raajjemv@users.noreply.github.com>
2021-01-11 20:25:18 +0500

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,7 +16,17 @@
      <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
      <link href="{{ asset('css/gallery/gallery.css') }}" rel="stylesheet">
      <link href="{{ asset('css/gallery/gallery-keyframes-swing.css') }}" rel="stylesheet">
+     <!-- Google Analytics -->
+     <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+ga('create', 'UA-187050657', 'auto');
+ga('send', 'pageview');
+     </script>
+     <!-- End Google Analytics -->
      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
      <style>
           .snap {
