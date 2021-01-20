a988a94ff37f706c4f3da7429fba224964283d1d
raajjemv <36122879+raajjemv@users.noreply.github.com>
2021-01-20 15:17:24 +0500

    gallery fix

--- resources/views/gallery-show.blade.php
+++ resources/views/gallery-show.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.main')
 @section('content')
-<div class="w-full">
+<div class="w-full bg-black">
      <div class="splide" data-splide='{"type":"loop"}'>
           <div class="splide__track">
                <ul class="splide__list">
                     @foreach ($gallery as $item)
                     <li class="splide__slide relative">
-                         <img src="{{ asset('storage/'.$item->image) }}" class="w-full h-screen object-cover">
+                         <img src="{{ asset('storage/'.$item->image) }}" class="w-full h-screen object-contain">
                          <div class="absolute bottom-8 w-full text-3xl text-white text-center">{{ $item->caption }}
                          </div>
                     </li>
