8adf86fd4e9f8842bbacea8613ed9b2ea2796238
raajjemv <36122879+raajjemv@users.noreply.github.com>
2021-06-26 13:13:27 +0500

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,17 +16,18 @@
      <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
      <link href="{{ asset('css/gallery/gallery.css') }}" rel="stylesheet">
      <link href="{{ asset('css/gallery/gallery-keyframes-swing.css') }}" rel="stylesheet">
-     <!-- Google Analytics -->
-     <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'G-XN402WJ693', 'auto');
-ga('send', 'pageview');
+
+     <!-- Global site tag (gtag.js) - Google Analytics -->
+     <script async src="https://www.googletagmanager.com/gtag/js?id=G-QVZZFGE3Q9"></script>
+     <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-QVZZFGE3Q9');
      </script>
-     <!-- End Google Analytics -->
+
      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
      <style>
           .snap {
